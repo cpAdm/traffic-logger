23bc9f0cbd1507ee90163e2c0838dbd1452da751
cpadm <57954026+cpAdm@users.noreply.github.com>
2024-11-23 22:14:06 +0100

    feat: Update to better UX/UI with icons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
-  <form @submit.prevent="handleSubmit">
+  <h1>Logged {{ activeEntries.length }} entries</h1>
+
+  <form @submit.prevent="addVehicle">
     <fieldset>
       <legend>Vehicle Type</legend>
-      <div>
-        <div v-for="option in vehicleTypes" :key="option">
-          <label>
-            <input v-model="vehicleType" :value="option" type="radio" />
-            {{ capitalize(option) }}
-          </label>
-        </div>
-      </div>
+      <label v-for="option in vehicleTypes" :key="option">
+        <input v-model="vehicleType" :value="option" type="radio" />
+        <CarIcon v-if="option === 'car'" />
+        <BusSideIcon v-else-if="option === 'bus'" />
+        <BikeIcon v-else-if="option === 'cyclist'" />
+        <WalkIcon v-else-if="option === 'pedestrian'" />
+        {{ capitalize(option) }}
+      </label>
     </fieldset>
 
     <fieldset>
       <legend>Direction</legend>
-      <div v-for="option in directions" :key="option">
-        <label>
-          <input v-model="direction" :value="option" type="radio" />
-          {{ capitalize(option) }}
-        </label>
-      </div>
+      <label v-for="option in directions" :key="option">
+        <input v-model="direction" :value="option" type="radio" />
+        <ArrowLeftIcon v-if="option === 'left'" />
+        <ArrowUpIcon v-else-if="option === 'straight'" />
+        <ArrowRightIcon v-else-if="option === 'right'" />
+        {{ capitalize(option) }}
+      </label>
     </fieldset>
 
-    <button type="submit">Add</button>
+    <button type="submit">Add entry</button>
   </form>
 
-  <button style="margin-top:1rem" @click="downloadCSV">Download CSV</button>
+  <button class="download" @click="downloadCSVData()">Download CSV</button>
+  <button class="download" @click="downloadJSONData()">Download JSON</button>
 
   <table>
     <thead>
@@ -65,32 +69,31 @@
         <td>{{ entry.timestamp }}</td>
 
         <td>
-          <button :disabled="entry.removed" @click="deleteEntry(entry.id)">Delete</button>
+          <button :disabled="entry.removed" class="error" @click="deleteEntry(entry.id)">
+            Delete
+          </button>
         </td>
       </tr>
     </tbody>
   </table>
 
-  <h2>Data as CSV</h2>
-  <pre><code>{{ textCSV }}</code></pre>
-
-  <h2>Data as JSON</h2>
-  <pre><code>{{
-      JSON.stringify(
-        entries.filter((entry) => !entry.removed),
-        undefined,
-        2
-      )
-    }}</code></pre>
-
-  <button @click="deleteAllEntries">Reset</button>
+  <button class="error" @click="deleteAllEntries">Reset all data</button>
 </template>
 
 <script lang="ts" setup>
 import { computed, onMounted, ref } from 'vue'
+import ArrowUpIcon from '@/assets/ArrowUpIcon.vue'
+import ArrowLeftIcon from '@/assets/ArrowLeftIcon.vue'
+import ArrowRightIcon from '@/assets/ArrowRightIcon.vue'
+import CarIcon from '@/assets/CarIcon.vue'
+import BusSideIcon from '@/assets/BusSideIcon.vue'
+import WalkIcon from '@/assets/WalkIcon.vue'
+import BikeIcon from '@/assets/BikeIcon.vue'
 
-const directions = ['left', 'right', 'straight']
+const directions = ['left', 'straight', 'right']
+const defaultDirection = 'straight'
 const vehicleTypes = ['car', 'bus', 'cyclist', 'pedestrian']
+const defaultVehicleType = 'car'
 
 type Entry = {
   id: string
@@ -100,24 +103,25 @@ type Entry = {
   removed?: boolean
 }
 
-const direction = ref('')
-const vehicleType = ref('')
+const direction = ref(defaultDirection)
+const vehicleType = ref(defaultVehicleType)
 
 const entries = ref<Entry[]>([])
+const activeEntries = computed(() => entries.value.filter((entry) => !entry.removed))
 
-const loadEntries = () => {
+function loadEntries() {
   const storedEntries = localStorage.getItem('trafficEntries')
   if (storedEntries) {
     entries.value = JSON.parse(storedEntries)
   }
 }
 
-const saveEntries = (newEntries: Entry[]) => {
+function saveEntries(newEntries: Entry[]) {
   entries.value = newEntries
   localStorage.setItem('trafficEntries', JSON.stringify(newEntries))
 }
 
-const handleSubmit = () => {
+function addVehicle() {
   if (direction.value && vehicleType.value) {
     const newEntry = {
       id: Date.now().toString(),
@@ -126,12 +130,12 @@ const handleSubmit = () => {
       timestamp: new Date().toLocaleString(),
     }
     saveEntries([newEntry, ...entries.value])
-    direction.value = 'straight'
-    vehicleType.value = 'car'
+    direction.value = defaultDirection
+    vehicleType.value = defaultVehicleType
   }
 }
 
-const updateEntry = (id: string, field: keyof Entry, eventTarget: EventTarget | null) => {
+function updateEntry(id: string, field: keyof Entry, eventTarget: EventTarget | null) {
   if (!eventTarget) {
     return
   }
@@ -143,49 +147,52 @@ const updateEntry = (id: string, field: keyof Entry, eventTarget: EventTarget | 
   saveEntries(updatedEntries)
 }
 
-const deleteEntry = (id: string) => {
+function deleteEntry(id: string) {
   const updatedEntries = entries.value.map((entry) =>
     entry.id === id ? { ...entry, removed: true } : entry,
   )
   saveEntries(updatedEntries)
 }
 
-const deleteAllEntries = () => {
+function deleteAllEntries() {
   if (window.confirm('Do you really want to DELETE ALL DATA?')) {
     saveEntries([])
   }
 }
 
-const capitalize = (string: string) => string.charAt(0).toUpperCase() + string.slice(1)
+function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
-const textCSV = computed(() => {
+function downloadData(data: BlobPart, type: string) {
+  const blob = new Blob([data], { type: type })
+
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.download = 'traffic-log'
+  link.click()
+
+  URL.revokeObjectURL(link.href)
+}
+
+function downloadJSONData() {
+  const data = JSON.stringify(activeEntries.value, undefined, 2)
+  downloadData(data, ' application/json')
+}
+
+function downloadCSVData() {
   const headers: (keyof Entry)[] = ['id', 'direction', 'vehicleType', 'timestamp']
 
   // Map each entry to a CSV row
-  const rows = entries.value
-    .filter((entry) => !entry.removed)
-    .map((entry) =>
-      headers
-        .map((header) => {
-          // Use double quotes to escape comma's
-          return `"${entry[header]}"`
-        })
-        .join(','),
-    )
+  const rows = activeEntries.value.map((entry) =>
+    // Use double quotes to escape comma's
+    headers.map((header) => `"${entry[header]}"`).join(','),
+  )
 
   // Combine the headers and rows into a CSV string
-  return [headers.join(','), ...rows].join('\n')
-})
+  const textCSV = [headers.join(','), ...rows].join('\n')
 
-const downloadCSV = () => {
-  const blob = new Blob([textCSV.value], { type: 'text/csv' });
-
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = "traffic-log";
-  link.click();
-
-  URL.revokeObjectURL(link.href);
+  downloadData(textCSV, 'text/csv')
 }
 
 onMounted(loadEntries)
@@ -197,6 +204,52 @@ onMounted(loadEntries)
 }
 
 label {
+  cursor: pointer;
+  display: block;
   font-size: 2rem;
+
+  svg {
+    width: 2rem;
+    vertical-align: bottom;
+  }
+
+  input[type='radio'] {
+    display: none; /* Hide default radio button */
+  }
+
+  input:checked + svg {
+    fill: #0083ef;
+  }
+}
+
+button {
+  cursor: pointer;
+  border-radius: 1rem;
+  background-color: transparent;
+
+  &[type='submit'] {
+    color: #0083ef;
+    border: 2px solid #0083ef;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    margin: 1rem 1rem 1rem 0;
+  }
+
+  &.download {
+    padding: 0.5rem 1rem;
+    margin: 1rem 1rem 1rem 0;
+  }
+
+  &.error {
+    border-radius: 5px;
+    color: #ef000c;
+    border-color: #ef000c;
+
+    &:disabled {
+      color: darkgrey;
+      border-color: darkgrey;
+      text-decoration: line-through;
+    }
+  }
 }
 </style>
